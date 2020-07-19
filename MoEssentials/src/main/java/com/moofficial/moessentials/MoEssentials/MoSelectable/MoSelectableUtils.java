fb44f42c5f8fe4d135da9fb53341dde01d752d44
Moh <moh10moh78@gmail.com>
2020-07-18 22:23:04 -0700

    updated MoSelectableUtils where now two functions were overloaded to take a MoListSelectable and set its size accordingly

--- MoEssentials/src/main/java/com/moofficial/moessentials/MoEssentials/MoSelectable/MoSelectableUtils.java
+++ MoEssentials/src/main/java/com/moofficial/moessentials/MoEssentials/MoSelectable/MoSelectableUtils.java
@@ -6,28 +6,50 @@ public class MoSelectableUtils {
      * selects all the items of the iterable
      * @param items iterable of selectable items
      */
-    public static void selectAllItems(Iterable<? extends MoSelectableItem> items){
+    private static void selectAllItems(Iterable<? extends MoSelectableItem> items){
         turnAllItems(true,items);
     }
 
+    /**
+     * selects all the items
+     * @param items
+     * @param selectable
+     */
+    private static void selectAllItems(Iterable<? extends MoSelectableItem> items,MoListSelectable selectable){
+        selectable.setSelectedSize(turnAllItems(true,items));
+    }
 
     /**
      * deselects all the items of the iterable
      * @param items iterable of selectable items
      */
-    public static void deselectAllItems(Iterable<? extends MoSelectableItem> items){
+    private static void deselectAllItems(Iterable<? extends MoSelectableItem> items){
         turnAllItems(false,items);
+    }
+
+    /**
+     * sets the selected size to 0 as well
+     * @param items
+     * @param selectable
+     */
+    private static void deselectAllItems(Iterable<? extends MoSelectableItem> items,MoListSelectable selectable){
+        deselectAllItems(items);
+        selectable.setSelectedSize(0);
     }
 
     /**
      * changes the selected state to the boolean given
      * @param b
      * @param items
+     * @return number of items inside items
      */
-    private static void turnAllItems(boolean b,Iterable<? extends MoSelectableItem> items){
+    private static int turnAllItems(boolean b,Iterable<? extends MoSelectableItem> items){
+        int i = 0;
         for(MoSelectableItem s : items){
             s.setSelected(b);
+            i++;
         }
+        return i;
     }
 
 
